47e603b08a521d993c34204280802b0c41d8df6d
Donald Le <ledinhcuong99@gmail.com>
2023-03-23 09:21:32 +0700

    Revert code to initial code

--- main.go
+++ main.go
@@ -12,32 +12,17 @@ func main() {
 	r := gin.New()
 
 	r.GET("/consuming", func(ctx *gin.Context) {
-		retryTimes := 0
 
-		for i := 1; i < 5; i++ {
-
-			response, err := http.Get(fmt.Sprintf("http://%s:%s/", os.Getenv("PROVIDER_URL"), os.Getenv("PROVIDER_PORT")))
-			if err != nil {
-				fmt.Printf("error making http request: %s\n", err)
-				os.Exit(1)
-			}
-
-			if response.StatusCode == 200 {
-				break
-			} else if response.StatusCode == 500 {
-
-				retryTimes += 1
-
-			}
-
+		response, err := http.Get(fmt.Sprintf("http://%s:%s/", os.Getenv("PROVIDER_URL"), os.Getenv("PROVIDER_PORT")))
+		if err != nil {
+			fmt.Printf("error making http request: %s\n", err)
+			os.Exit(1)
 		}
 
-		if retryTimes >= 4 {
-
-			ctx.String(500, "Internal server error")
-
-		} else {
+		if response.StatusCode == 200 {
 			ctx.String(200, "Successfully consuming message")
+		} else {
+			ctx.String(500, "Internal server error")
 		}
 
 	})
